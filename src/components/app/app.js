3ab93874c6483273347fae528541e5163ef34751
Оксана <106163937+GurinAOA@users.noreply.github.com>
2024-08-02 14:40:25 +0300

    used props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,29 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
+import "./app.css";
 
 function App() {
+  const data = [
+    { name: "John", salary: 1000, increase: false },
+    { name: "ALex", salary: 2000, increase: true},
+    { name: "Oleg", salary: 10000, increase: false },
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data}/>
+      <EmployeesAddForm />
     </div>
   );
 }
